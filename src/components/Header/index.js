0dea547b0467d57dc428bc2070b72cd82e98ca96
Kened Oliveira <kenedwanderson@live.com>
2019-12-22 01:24:15 -0300

    feat: add signout

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,16 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { GoSignOut } from 'react-icons/go';
+import { useDispatch, useSelector } from 'react-redux';
 
-import { Container } from './styles';
+import { signOut } from '~/store/modules/auth/actions';
+
+import { Container, SignOut } from './styles';
 
 import logo from '~/assets/imgs/logo.png';
 
 export default function Header() {
+  const dispatch = useDispatch();
+  const isAuth = useSelector(state => state.auth.signed);
+
+  function handleSignOut() {
+    dispatch(signOut());
+  }
+
   return (
     <Container>
       <Link to="/">
         <img src={logo} from="" alt="Home" />
       </Link>
+      {isAuth && (
+        <SignOut type="button" onClick={handleSignOut}>
+          <GoSignOut />
+          SignOut
+        </SignOut>
+      )}
     </Container>
   );
 }
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -2,13 +2,30 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   display: flex;
-  align-items: center;
   justify-content: center;
+  align-items: center;
   margin: 10px 0;
+  border-bottom: 1px solid #d3d3d3;
 
   > a {
+    margin: auto;
     img {
       max-width: 150px;
     }
+  }
+`;
+
+export const SignOut = styled.button`
+  display: flex;
+  align-self: flex-end;
+  position: absolute;
+  right: 0;
+  border: none;
+  margin: 0px 10px;
+  color: #3d3d3d;
+  > svg {
+    max-width: 20px;
+    max-height: 20px;
+    margin-right: 5px;
   }
 `;
